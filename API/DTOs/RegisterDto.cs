58ebc09bf0646322d20e244e42a717770b106f83
Igor Tatarinov <igor.tanv@gmail.com>
2021-10-06 23:36:19 -0400

    add login endpoint

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,13 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs
 {
-    public class RegisterDto
+  public class RegisterDto
     {
+        [Required]
         public string Username { get; set; }
+         
+         [Required]
         public string Password { get; set; }
         
     }
