59f7bd8e1008f56e369a14bc39e1cdacba114ef1
Igor Tatarinov <igor.tanv@gmail.com>
2021-10-10 14:57:13 -0400

    initialized new angular client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,25 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-  
-
-  title = 'Dating App';
-  users: any;
-
-  constructor(private http: HttpClient) { }
-
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe((response: any) => {
-      this.users = response
-    }, error => {
-      console.log(error)
-    })
-  }
+export class AppComponent {
+  title = 'client';
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,25 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
