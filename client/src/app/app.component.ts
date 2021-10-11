d37c5461c1958f1b3c9b1135c5dda49e23175bfc
Igor Tatarinov <igor.tanv@gmail.com>
2021-10-10 20:52:36 -0400

    added ssl cert

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+
+  title = 'dating app';
+  users: any 
+
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe(res => {
+      console.log(res)
+    })
+  }
+
+  
+
 }
