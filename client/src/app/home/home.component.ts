a83386a66f3918be8d05658097ebb50b8141da6a
Igor Tatarinov <igor.tanv@gmail.com>
2021-10-11 13:53:22 -0400

    add cancel registration

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,14 +9,24 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   registerMode = false;
+  users: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getUsers()
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(res => this.users = res);
+  }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
   }
 
 }
