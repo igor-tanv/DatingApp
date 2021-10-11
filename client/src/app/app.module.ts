4f97762b793fe31d756114143d1bbd5eb2fe45c6
Igor Tatarinov <igor.tanv@gmail.com>
2021-10-10 21:04:23 -0400

    add: navbar, bootstrap, font-awesome

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
